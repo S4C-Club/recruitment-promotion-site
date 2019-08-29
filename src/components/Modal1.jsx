9fd3a586462a018378533d62cce004e4e4825e20
arima <tronghung199@gmail.com>
2019-08-29 21:20:38 +0700

    fix bug about bootstrap

--- src/components/Modal1.jsx
+++ src/components/Modal1.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import img from "../assests/smtt.png";
+
 export default class ImageComponent extends Component {
   state = { isOpen: false };
 
@@ -9,35 +11,53 @@ export default class ImageComponent extends Component {
 
   render() {
     return (
-      <div >
-    
-    <div class = "position-relative">
-    <button type="button" className="btn btn-secondary btn-sm position-absolute" data-toggle="modal" data-target="#myModal">Read More</button> 
-    <img src = "/assests/smtt.png.jpg" class="img-fluid" id="img" alt="Responsive image"/>
-  </div>
-  
-  <div className="modal fade" id="myModal" role="dialog">
-    <div className="modal-dialog">
-    
-      <div className="modal-content">
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal">&times;</button>
-          <h4 className="modal-title">Sắc màu tuổi thơ</h4>
+      <div>
+        <div class="position-relative">
+          <button
+            type="button"
+            className="btn btn-secondary btn-sm position-absolute"
+            data-toggle="modal"
+            data-target="#myModal"
+          >
+            Read More
+          </button>
+          <img
+            src={img}
+            class="img-fluid"
+            id="img"
+            alt="Responsive"
+          />
         </div>
-        <div className="modal-body">
-          <img src="/assests/smtt.png.jpg" class="img-fluid" alt="Responsive image"/>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+
+        <div className="modal fade" id="myModal" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">
+                  &times;
+                </button>
+                <h4 className="modal-title">Sắc màu tuổi thơ</h4>
+              </div>
+              <div className="modal-body">
+                <img
+                  src={img}
+                  class="img-fluid"
+                  alt="Responsive"
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
-    </div>
-  </div>
-  
-</div>
-
-
     );
   }
 }
--- src/components/Modal2.jsx
+++ src/components/Modal2.jsx
@@ -1,33 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import img from "../assests/TIESTO1.OFF.png";
 
 export default class Modal2 extends Component {
-    render() {
-        return (
-            <div>
-                <div class = "position-relative">
-    <button type="button" className="btn btn-secondary btn-sm position-absolute" data-toggle="modal" data-target="#myModal2">Read More</button> 
-      <img src = "/assests/TIESTO1.OFF.jpg" class="img-fluid" id="img" alt="Responsive image"/>
-  </div>
-  
-  <div className="modal fade" id="myModal2" role="dialog">
-    <div className="modal-dialog">
-    
-      <div className="modal-content">
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal">&times;</button>
-          <h4 className="modal-title">emBee Festival</h4>
+  render() {
+    return (
+      <div>
+        <div class="position-relative">
+          <button
+            type="button"
+            className="btn btn-secondary btn-sm position-absolute"
+            data-toggle="modal"
+            data-target="#myModal2"
+          >
+            Read More
+          </button>
+          <img
+            src={img}
+            class="img-fluid"
+            id="img"
+            alt="Responsive"
+          />
         </div>
-        <div className="modal-body">
-          <img src="/assests/tiesto.jpg.jpg" class="img-fluid" alt="Responsive image"/>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+
+        <div className="modal fade" id="myModal2" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">
+                  &times;
+                </button>
+                <h4 className="modal-title">emBee Festival</h4>
+              </div>
+              <div className="modal-body">
+                <img
+                  src={img}
+                  class="img-fluid"
+                  alt="res"
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
-    </div>
-  </div>
-            </div>
-        )
-    }
+    );
+  }
 }
