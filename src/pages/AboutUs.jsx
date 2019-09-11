b0c76a7a8e36e2d3f4f6ce05eee375d3df701559
arima <tronghung199@gmail.com>
2019-09-11 12:48:01 +0700

    update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Header from "../components/Header";
 import BlockOtherEvents from "../components/BlockOtherEvents";
 import BlockAboutUs from "../components/BlockAboutUs";
 export default class AboutUs extends Component {
